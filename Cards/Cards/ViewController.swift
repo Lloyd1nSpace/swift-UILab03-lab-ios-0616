41aa1eb42981383e60006b90f21ab0417413cf8c
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-07-09 02:49:33 -0700

    Keep keeping on

--- Cards/Cards/ViewController.swift
+++ Cards/Cards/ViewController.swift
@@ -9,11 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-    // TODO: Create IB outlets
 
+    @IBOutlet weak var topLeftLabel: UILabel!
+    @IBOutlet weak var centerLabel: UILabel!
+    @IBOutlet weak var bottomRightLabel: UILabel!
+
+    @IBOutlet weak var fourOfClubsButton: UIButton!
+    @IBOutlet weak var threeOfSpadesButton: UIButton!
+    @IBOutlet weak var eightOfDiamondsButton: UIButton!
+    @IBOutlet weak var tenOfHeartsButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    // TODO: IB actions and code to update UI
+    
+   // It was hinted that this could be done with one function??? Hmm...
+    
+    @IBAction func fourOfClubsButtonTapped(sender: AnyObject) {
+        topLeftLabel.text = "♣️"
+        bottomRightLabel.text = "♣️"
+        centerLabel.text = "4"
+    }
+    
+    @IBAction func threeOfSpadesButtonTapped(sender: AnyObject) {
+        topLeftLabel.text = "♠️"
+        bottomRightLabel.text = "♠️"
+        centerLabel.text = "3"
+    }
+    
+    @IBAction func eightOfDiamondsButtonTapped(sender: AnyObject) {
+        topLeftLabel.text = "♦️"
+        bottomRightLabel.text = "♦️"
+        centerLabel.text = "8"
+    }
+    
+    @IBAction func tenOfHeartsButtonTapped(sender: AnyObject) {
+        topLeftLabel.text = "♥️"
+        bottomRightLabel.text = "♥️"
+        centerLabel.text = "10"
+    }
 }
